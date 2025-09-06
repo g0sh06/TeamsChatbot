75cd8076033cba036a6597a05ab8ca3736582236
g0sh06 <s.atanasov51@gmail.com>
2025-09-06 19:53:35 +0200

    trust_remote_code=True

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 gpt4all_embeddings = HuggingFaceEmbeddings(
-    model_name="nomic-ai/nomic-embed-text-v1.5"
+    model_name="nomic-ai/nomic-embed-text-v1.5",
+    trust_remote_code=True
 )
